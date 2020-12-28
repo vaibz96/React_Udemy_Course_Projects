32b3cb20715d59ee80497daa04b5921910fd18bf
vaibz96 <vshanbha@stevens.edu>
2020-12-27 19:49:12 -0500

    My first JSX project

---
+++ jsx/src/index.js
@@ -0,0 +1,21 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom'; // bundled in webpack
+
+// Create a react component
+const App = () => {
+    const buttonText = { text: 'Click Me'}
+    const labelText = 'Enter Name:'
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                {labelText}
+            </label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'blue', color:'white', }}> {buttonText.text} </button>
+        </div>
+    );
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector('#root'));
