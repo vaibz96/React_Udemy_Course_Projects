cfa8f07af6416dfa9cea10f811b76c685d6dfca9
vaibz96 <vshanbha@stevens.edu>
2021-01-04 00:48:12 -0500

    my first simple react app using class based components and state

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,36 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
-const SeasonDisplay = () => {
-    return <div> Hi Seasons here</div>
+const seasonConfig = {
+    summer:{
+        text: 'Let\'s hit the beach!!',
+        icon: 'sun'
+    },
+    winter:{
+        text: 'Burr, it\'s Chilly',
+        icon: 'snowflake'
+    }
+}
+
+const getSeason = (latitude, month) =>{
+    if( month>2 && month<9 ) {
+        return latitude > 0 ? 'summer':'winter';
+    }else{
+        return latitude > 0 ? 'winter': 'summer';
+    }
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.latitude, new Date().getMonth());
+    const {text, iconName} = seasonConfig[season];
+
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
+            <h1> { text } </h1>
+            <i className={`icon-right massive ${iconName} icon`} />
+        </div>
+    );
 };
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 // Function based component
 // const App = () => {
@@ -11,17 +13,32 @@ import ReactDOM from 'react-dom';
 //     return <div> Hi There </div>;
 // };
 
+
 // Class based component
 class App extends React.Component {
-    render() {
+    state = { latitude: null, errorMessage: '' }
+
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            error => console.log(error)
-            
-            );
-        return <div> Hi There </div>;
-    }
-}
+            // to change the value dynamically we need to use setState
+            position => this.setState({ latitude: position.coords.latitude }),
+            error => this.setState({ errorMessage: error.message })
+        );
+    };
+
+    // Render the class based component (always)
+    render() {
+        if ( this.state.errorMessage && !this.state.latitude ) {
+            return <div> Error: { this.state.errorMessage }</div>
+        };
+
+        if ( !this.state.errorMessage && this.state.latitude ) {
+            return <SeasonDisplay latitude={ this.state.latitude } />
+        };
+
+        return <Spinner message="Please accept the location request"/>
+    };
+};
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
 
